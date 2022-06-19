169b48eedd0719d7a6a2080c9beebc36d8347563
Timo Loomets <tiloom@ttu.ee>
2022-06-19 11:12:17 +0300

    security package: MCU code V0

---
+++ codex2022.ino
@@ -0,0 +1,156 @@
+/*  
+ Test the tft.print() viz the libraries embedded write() function
+
+ This sketch used font 2, 4, 7
+ 
+ Make sure all the required fonts are loaded by editting the
+ User_Setup.h file in the TFT_eSPI library folder.
+
+  #########################################################################
+  ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
+  #########################################################################
+ */
+
+#include <SPI.h>
+
+#define EKRAAN
+
+#include <WiFi.h>
+
+const char* ssid     = "codex2022";
+const char* password = "codex2022";
+
+const char* host = "10.2.112.159";
+const int port = 5000;
+
+
+#ifdef EKRAAN
+
+#include <TFT_eSPI.h> // Hardware-specific library
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+#define TFT_GREY 0x5AEB // New colour
+#endif
+
+
+void setup(void) {
+    pinMode(25, INPUT_PULLUP);
+
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+  #ifdef EKRAAN
+  tft.init();
+  #endif
+  
+}
+
+// Use WiFiClient class to create TCP connections
+WiFiClient client;
+
+void loop() {
+  
+  // Fill screen with random colour so we can see the effect of printing with and without 
+  // a background colour defined
+  int a = digitalRead(25);
+  #ifdef EKRAAN
+  tft.fillScreen(a==0?TFT_GREEN:TFT_RED);
+  #endif
+  
+  if (!client.connected()){
+    Serial.print("connecting to ");
+    Serial.println(host);
+    if (!client.connect(host, port)) {
+        Serial.println("connection failed");
+        delay(1000);
+        return;
+    }
+  }
+
+  // This will send the request to the server
+  String url = "/";
+    url += "?pask=";
+    url += a==0?"1":"0";
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+                // "Content-Length: \r\n\r\n");
+  /*unsigned long timeout = millis();
+  while (client.available() == 0) {
+      if (millis() - timeout > 5000) {
+          Serial.println(">>> Client Timeout !");
+          client.stop();
+          return;
+      }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+  }*/
+
+  Serial.println();
+  Serial.println("closing connection");
+  
+  // Set "cursor" at top left corner of display (0,0) and select font 2
+  // (cursor will move to next line automatically during printing with 'tft.println'
+  //  or stay on the line is there is room for the text with tft.print)
+  /*tft.setCursor(0, 0, 2);
+  // Set the font colour to be white with a black background, set text size multiplier to 1
+  tft.setTextColor(TFT_WHITE,TFT_BLACK);  tft.setTextSize(1);
+  // We can now plot text on screen using the "print" class
+  tft.println("Hello World!");
+  
+  // Set the font colour to be yellow with no background, set to font 7
+  tft.setTextColor(TFT_YELLOW); tft.setTextFont(7);
+  tft.println(1234.56);
+  
+  // Set the font colour to be red with black background, set to font 4
+  tft.setTextColor(TFT_RED,TFT_BLACK);    tft.setTextFont(4);
+  tft.println((long)3735928559, HEX); // Should print DEADBEEF
+
+  // Set the font colour to be green with black background, set to font 4
+  tft.setTextColor(TFT_GREEN,TFT_BLACK);
+  tft.setTextFont(4);
+  tft.println("Groop");
+  tft.println("I implore thee,");
+
+  // Change to font 2
+  tft.setTextFont(2);
+  tft.println(F("my foonting turlingdromes.")); // Can store strings in FLASH to save RAM
+  tft.println("And hooptiously drangle me");
+  tft.println("with crinkly bindlewurdles,");
+  // This next line is deliberately made too long for the display width to test
+  // automatic text wrapping onto the next line
+  tft.println("Or I will rend thee in the gobberwarts with my blurglecruncheon, see if I don't!");
+  
+  // Test some print formatting functions
+  float fnumber = 123.45;
+   // Set the font colour to be blue with no background, set to font 4
+  tft.setTextColor(TFT_BLUE);    tft.setTextFont(4);
+  tft.print("Float = "); tft.println(fnumber);           // Print floating point number
+  tft.print("Binary = "); tft.println((int)fnumber, BIN); // Print as integer value in binary
+  tft.print("Hexadecimal = "); tft.println((int)fnumber, HEX); // Print as integer number in Hexadecimal
+  delay(10000);*/
+}
